5518e94a9847ec9f7a125a1c3581d11089944f23
Saurabh S <saurabh@appler.xyz>
2017-08-30 16:59:23 +0530

    fixed start process for dev environment

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dev'),
     publicPath: '/',
-    filename: '[name].[chunkhash].js',
+    filename: '[name].[hash].js',
   },
   plugins: [
     // Hash the files using MD5 so that their names change when the content changes.
